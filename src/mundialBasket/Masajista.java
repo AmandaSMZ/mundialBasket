7d51eaae92831e263931e38a5eec1a5332c524ab
amandaSMZ <amandamigut@gmail.com>
2024-03-29 18:17:18 +0100

    Completado

--- src/mundialBasket/Masajista.java
+++ src/mundialBasket/Masajista.java
@@ -1,37 +1,66 @@
 package mundialBasket;
-
+	/**
+	 * Hereda de Persona
+	 * @author amand
+	 * @version 2.0
+	 */
 public class Masajista extends Persona {
 	String titulacion;
 	int aniosExperiencia;
 	
+	/**
+	 * Constructor Masajista
+	 * @param id
+	 * @param nombre
+	 * @param apellidos
+	 * @param fechaNac
+	 * @param titulacion
+	 * @param aniosExperiencia
+	 */
 	public Masajista(int id, String nombre, String apellidos, String fechaNac, String titulacion,
 			int aniosExperiencia) {
 		super(id, nombre, apellidos, fechaNac);
 		this.titulacion = titulacion;
 		this.aniosExperiencia = aniosExperiencia;
 	}
-
-	protected String getTitulacion() {
+	/**
+	 * Método para obtener la titulacion
+	 * @return tipo de titulación (String)
+	 */
+	public String getTitulacion() {
 		return titulacion;
 	}
-
+	/**
+	 * Método para cambiar la titulación
+	 * @param titulacion Nombre de la nueva titulación (String)
+	 */
 	protected void setTitulacion(String titulacion) {
 		this.titulacion = titulacion;
 	}
-
+	/**
+	 * Método para obtener los años de experiencia
+	 * @return años de experiencia (int)
+	 */
 	protected int getAniosExperiencia() {
 		return aniosExperiencia;
 	}
-
+	/**
+	 * Método para cambiar los años de experiencia
+	 * @param aniosExperiencia Años de experiencia (int)
+	 */
 	protected void setAniosExperiencia(int aniosExperiencia) {
 		this.aniosExperiencia = aniosExperiencia;
 	}
-
+	
 	@Override
 	public void viajar() {
 		System.out.println("Viajando para preparar a los jugadores para el partido");
 		
 	}
+	/**
+	 * Método para dar masaje
+	 * Muestra un mensaje por pantalla
+	 */
 	public void darMasaje() {
 		System.out.println(this.nombre+" está dando un masaje");
 	}
--- src/mundialBasket/Principal.java
+++ src/mundialBasket/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Seleccion italia = new Seleccion();
-		
+		/*
 		italia.añadirIntegrante(new Jugador(25,"Pau","Gasol","1980-07-06",16,"Pivot"));
 		italia.añadirIntegrante(new Jugador(13,"David","García","1979-07-06",13,"Defensa"));
 		italia.añadirIntegrante(new Entrenador(456, "Juan","Rodriguez Vazquez", "1987-06-19", "Federacion de Madrid"));
@@ -13,7 +13,7 @@ public class Principal {
 		italia.darMasaje();
 		italia.dirigirEntrenamiento("2024-06-05");
 		italia.jugarPartido();
-
+		*/
 	}
 
 }
