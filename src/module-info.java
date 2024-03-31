970b7002340f5429d405cd17caab46a9f20b7144
amandaSMZ <amandamigut@gmail.com>
2024-03-31 17:58:14 +0200

    Añadido link a JavaDoc

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,8 @@
  * 
  */
 /**
- * <h2><u>Proyecto para la gestion del mundial de baloncesto de 2058</u></h2>
+ * <h2>Proyecto para la gestion del mundial de baloncesto de 2058</h2>
+ * <h1><u><a href="https://github.com/AmandaSMZ/mundialBasket">Link al Proyecto de GitHub</a></u></h1>
  */
 module mundialBasket {
 }
