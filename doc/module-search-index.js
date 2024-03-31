1169b72f16711332db50e6b29805ae7ddce4d6bf
amandaSMZ <amandamigut@gmail.com>
2024-03-31 15:54:24 +0200

    Corrección nombre método

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"mundialBasket"}];updateSearchResults();
--- src/mundialBasket/Principal.java
+++ src/mundialBasket/Principal.java
@@ -4,16 +4,16 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Seleccion italia = new Seleccion();
-		/*
-		italia.añadirIntegrante(new Jugador(25,"Pau","Gasol","1980-07-06",16,"Pivot"));
-		italia.añadirIntegrante(new Jugador(13,"David","García","1979-07-06",13,"Defensa"));
-		italia.añadirIntegrante(new Entrenador(456, "Juan","Rodriguez Vazquez", "1987-06-19", "Federacion de Madrid"));
-		italia.añadirIntegrante(new Masajista(166, "Lucia", "Sanchez", "1994-05-13","Grado Enfermería",5));
+		
+		italia.aniadirIntegrante(new Jugador(25,"Pau","Gasol","1980-07-06",16,"Pivot"));
+		italia.aniadirIntegrante(new Jugador(13,"David","García","1979-07-06",13,"Defensa"));
+		italia.aniadirIntegrante(new Entrenador(456, "Juan","Rodriguez Vazquez", "1987-06-19", "Federacion de Madrid"));
+		italia.aniadirIntegrante(new Masajista(166, "Lucia", "Sanchez", "1994-05-13","Grado Enfermería",5));
 		
 		italia.darMasaje();
 		italia.dirigirEntrenamiento("2024-06-05");
 		italia.jugarPartido();
-		*/
+		
 	}
 
 }
